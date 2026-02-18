320f35aef58d845362b821d521418a42616f5cf6
Nabin <sharmanabin.0910@gmail.com>
2026-02-18 11:37:02 +0530

    chore : designed and animated the hero section

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
+'use client'
+import {gsap , useGSAP , SplitText} from '@/lib/gsap-utils'
 import Button from './Button'
+import { useRef } from 'react'
+import { aboutStatusItmes } from '@/data/data'
 
 export default function About() {
-  return <section>
-    <div>
+
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+    useGSAP(()=>{
+        const textSplit = SplitText.create('.text' , {
+            type : 'words , lines',
+            linesClass : 'text-line'
+        })
+        const textSplitTl = gsap.timeline({
+            scrollTrigger : {
+                trigger: '.about-wrapper',
+                start : 'top 50%'
+            }
+        })
+        textSplitTl.from(textSplit.words, {
+            yPercent : 100,
+            duration : 1,
+            stagger : 0.02 ,
+            ease : 'power2.inout'
+        })
+        const tl = gsap.timeline({
+            scrollTrigger : {
+                trigger : containerRef.current,
+                start : 'top center',
+                end : 'bottom center',
+                scrub : 1
+            }
+        })
+        tl.to(wrapperRef.current , {
+            maxWidth : '100%',
+            duration : 1,
+            ease : 'power3.out'
+        })
+    },{
+        scope : containerRef
+    })
+
+  return <section ref={containerRef} className='bg-stone-900  min-h-svh pt-14'>
+    <div className='text-center space-y-5 sm:space-y-7'>
         {/* Wrapper */}
-        <div className="">
-            <h2 className=''>Abour Me</h2>
-            <p>I am Nabin Sharma , a full stack developer and a competitive programmer,
+        <div ref={ wrapperRef} className=" about-wrapper bg-stone-100 mx-auto max-w-[90%] sm:max-w-[70%] py-20 h-dvh rounded-t-[100px] px-8 space-y-5">
+            <h2 className=' text text-4xl sm:text-5xl lg:text-7xl '>About Me</h2>
+            <p className='text text-lg lg:text-xl max-w-3xl mx-auto '>I am Nabin Sharma , a full stack developer and a competitive programmer,
             I build scalable , maintainable web applications. I also love to study Algorithms(it is not dead).
             </p>
-            <p>
+            <p className=' text max-w-3xl  mx-auto'>
                 I specialise in developing using the JavaScript Ecosystem , using NodeJs(as my runtime) , 
                 ExpressJs and ReactJs as my primary frameworks , I have worked on SQL as well as 
                 NoSQL Databases , I aim to create high quality and efficient softwares that could make
@@ -19,7 +60,17 @@ export default function About() {
             {/* Btn */}
             <Button label='View Projects' />
             {/* Status */}
-            
+
+            <div className="grid gap-4 sm:grid-cols-2 mt-10 max-w-4xl mx-auto">
+                {aboutStatusItmes.map(item => (
+                    <div key={item.id} className='border border-stone-300 p-4'>
+                        <span className=' text text-xl sm:text-2xl md:text-3xl font-bebasNeue'>
+                            {item.value}
+                        </span>
+                        <p className='text text-stone-600'>{item.label}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     </div>
   </section>
